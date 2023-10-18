9a65ed5955ff9d26458180add3577eea65813b79
Michael Womack <michael.womack84@gmail.com>
2023-10-18 11:18:32 -0400

    add code to RESTAPITest from git X dev

--- src/test/java/com/womack/Mavenjava/RESTAPITest.java
+++ src/test/java/com/womack/Mavenjava/RESTAPITest.java
@@ -7,6 +7,8 @@ public class RESTAPITest {
 	@Test(groups= {"smoke"})
 	public void postJira() {
 		System.out.println("postJira");
+		System.out.println("postJira2");
+		System.out.println("postJira3");
 	}
 	
 	@Test 

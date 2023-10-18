78da8cfa67c284265899983d185462ae3d66c3c3
Michael Womack <michael.womack84@gmail.com>
2023-10-18 14:30:10 -0400

    add new Demo test

--- src/test/java/com/womack/Mavenjava/Demo.java
+++ src/test/java/com/womack/Mavenjava/Demo.java
@@ -1,5 +1,12 @@
 package com.womack.Mavenjava;
 
+import org.testng.annotations.Test;
+
 public class Demo {
 
+	@Test
+	public void demoTest() {
+		System.out.println("Demo class test");
+		System.out.println("Demo class test2");
+	}
 }
